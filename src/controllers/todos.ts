1c18d1f2930f5cfeef4ed30874f1040b8256ae0e
진찬호 <chanhojin94@kakao.com>
2024-01-03 21:31:55 +0900

    final

---
+++ src/controllers/todos.ts
@@ -0,0 +1,48 @@
+import { RequestHandler } from "@nestjs/common/interfaces";
+import { NextFunction, Request, Response } from "express";
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo = (req: Request, res: Response, next: NextFunction) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+
+  res.status(201).json({ message: "Created the todo", createdTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo = (req: Request, res: Response, next: NextFunction) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Todo deleted" });
+};
